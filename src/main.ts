bfb9c1ef2d01aeb5c7696d7022c5fc9ef8bfd337
iainfox <furghadmanar@gmail.com>
2025-11-18 22:29:24 -0600

    rotate test image towards mouse

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,20 @@ if (!image) {
 const sprite = new Sprite(image)
 sprite.go_to_xy(0, 0)
 
+let mouseX = 0;
+let mouseY = 0;
+
+window.onmousemove = (event: MouseEvent) => {
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    mouseX = event.clientX - centerX;
+    mouseY = centerY - event.clientY;
+    
+    sprite.point_in_direction_xy(mouseX, mouseY);
+};
+
 setInterval(() => {
-  sprite.move_x_steps(10)
-  sprite.change_y_by(-1)
+    sprite.point_in_direction_xy(mouseX, mouseY);
+    sprite.move_x_steps(10)
+    sprite.change_y_by(-1)
 }, 1000)
