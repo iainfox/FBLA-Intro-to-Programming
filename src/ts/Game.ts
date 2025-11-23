9025d3faa91a0cb5345b9c6dd13f144c5128bc77
iainfox <furghadmanar@gmail.com>
2025-11-22 21:27:25 -0600

    add game class with a backdrop sprite

---
+++ src/ts/Game.ts
@@ -0,0 +1,31 @@
+import Sprite from 'Sprite'
+
+export default class Game {
+    backdrop: Sprite
+
+    constructor (backdrop: Sprite) {
+        this.backdrop = backdrop
+    }
+
+    /**
+     * Adds a new backdrop image to the backdrop.
+     * 
+     * @param {string} backdrop_name
+     * the name of the backdrop
+     * @param {string} image_url
+     * the URL of the backdrop image
+     */
+    public add_backdrop(backdrop_name: string, image_url: string) {
+        this.backdrop.add_costume(backdrop_name, image_url)
+    }
+
+    /**
+     * Switches the backdrop's iamge to the given backdrop name if it exists.
+     * 
+     * @param {string} backdrop_name
+     * The name of the backdrop to switch to.
+     */
+    public switch_bostume(backdrop_name: string) {
+        this.backdrop.switch_costume(backdrop_name)
+    }
+}
