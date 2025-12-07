1e17eca4428152f537023c87152262b41b1a3256
iainfox <furghadmanar@gmail.com>
2025-12-06 19:38:02 -0600

    add image loading helper function

--- src/test/main.ts
+++ src/test/main.ts
@@ -1,6 +1,15 @@
 import Sprite from '../ts/Sprite.js'
 import Game from '../ts/Game.js'
 
+async function load_image(img_path: string) : Promise<HTMLImageElement> {
+    const image = new Image();
+    return new Promise<HTMLImageElement>((resolve, reject) => {
+        image.onload = () => resolve(image);
+        image.onerror = reject;
+        image.src = "../../temp.jpg";
+    });
+}
+
 const canvas = document.getElementById("a") as HTMLCanvasElement | null;
 if (!canvas) throw new Error("Canvas element with id 'a' not found.");
 
