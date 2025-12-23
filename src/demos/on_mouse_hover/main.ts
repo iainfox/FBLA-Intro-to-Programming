60750d9c376a66406905dde27582df9f4feb6306
iainfox <furghadmanar@gmail.com>
2025-12-22 19:51:46 -0600

    Use game class helper image loader method

--- src/demos/on_mouse_hover/main.ts
+++ src/demos/on_mouse_hover/main.ts
@@ -1,16 +1,7 @@
 import Sprite from '../../ts/Sprite.js'
 import Game from '../../ts/Game.js'
 
-async function load_image(img_path: string) : Promise<HTMLImageElement> {
-    const image = new Image();
-    return new Promise<HTMLImageElement>((resolve, reject) => {
-        image.onload = () => resolve(image);
-        image.onerror = reject;
-        image.src = img_path;
-    });
-}
-
-const image = await load_image("../../../temp.jpg");
+const image = await Game.load_image("../../../temp.jpg");
 
 const sprite = new Sprite(image, "costume1");
 sprite.set_size_to_x(100);
